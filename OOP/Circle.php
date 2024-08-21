29f00655f0a17d5d659db81661d4b19fa0fd06bd
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-08-21 22:25:43 +0300

    Adding Documentation

--- OOP/Circle.php
+++ OOP/Circle.php
@@ -1,61 +1,134 @@
 <?php
+/**
+ * Class `Circle` that defines a circle
+ */
 class Circle
 {
+	/** @var float $radius Public instance attribute: radius */
 	public float $radius = 1;
+
+	/** @var string $color Public instance attribute: color */
 	public string $color = "red";
 
+	/**
+	 * Constructor method for the `Circle` class
+	 * 
+	 * @param float $radius The radius of the circle
+	 * @param string $color The color of the circle
+	 */
 	function __construct(float $radius, string $color)
 	{
 		$this->radius = $radius;
 		$this->color = $color;
 	}
-	function set_color(string $radius): void
-	{
-		$this->color = $radius;
-	}
-	function get_color($color)
+
+	/**
+	 * Set the color of the circle
+	 * 
+	 * @param string $color The new color of the circle
+	 * @return void
+	 */
+	function set_color(string $color): void
 	{
 		$this->color = $color;
 	}
 
-	function set_radius($radius)
+	/**
+	 * Get the color of the circle
+	 * 
+	 * @return string The current color of the circle
+	 */
+	function get_color(): string
+	{
+		return $this->color;
+	}
+
+	/**
+	 * Set the radius of the circle
+	 * 
+	 * @param float $radius The new radius of the circle
+	 * @return void
+	 */
+	function set_radius(float $radius): void
 	{
 		$this->radius = $radius;
 	}
 
-	function get_radius()
+	/**
+	 * Get the radius of the circle
+	 * 
+	 * @return float The current radius of the circle
+	 */
+	function get_radius(): float
 	{
 		return $this->radius;
 	}
 
-	function get_area()
+	/**
+	 * Calculate the area of the circle
+	 * 
+	 * @return float The area of the circle
+	 */
+	function get_area(): float
 	{
 		return pi() * pow($this->radius, 2);
 	}
 }
-$circle = new Circle("1", "red");
+
+$circle = new Circle(1, "red");
 print_r($circle);
 
+/**
+ * Class `Cyliner` that defines a cylinder and extends the `Circle` class
+ */
 class Cyliner extends Circle
 {
+	/** @var float $height Public instance attribute: height */
 	public float $height = 1;
 
+	/**
+	 * Constructor method for the `Cyliner` class
+	 * 
+	 * @param float $radius The radius of the cylinder's base (inherited from Circle)
+	 * @param float $height The height of the cylinder
+	 * @param string $color The color of the cylinder (inherited from Circle)
+	 */
 	function __construct(float $radius, float $height, string $color)
 	{
+		parent::__construct($radius, $color);
 		$this->height = $height;
 	}
-	function set_height($height)
+
+	/**
+	 * Set the height of the cylinder
+	 * 
+	 * @param float $height The new height of the cylinder
+	 * @return void
+	 */
+	function set_height(float $height): void
 	{
 		$this->height = $height;
 	}
 
-	function get_height()
+	/**
+	 * Get the height of the cylinder
+	 * 
+	 * @return float The current height of the cylinder
+	 */
+	function get_height(): float
 	{
 		return $this->height;
 	}
 
-	function get_volume($height)
+	/**
+	 * Calculate the volume of the cylinder
+	 * 
+	 * @return float The volume of the cylinder
+	 */
+	function get_volume(): float
 	{
-		return $this->get_area() * $height;
+		return $this->get_area() * $this->height;
 	}
 }
+
+?>
--- OOP/Person.php
+++ OOP/Person.php
@@ -1,33 +1,73 @@
 <?php
 
+/**
+ * Class `Person` represents a person with a name and address.
+ */
 class Person
 {
+	/** @var string $name The name of the person */
 	public string $name;
+
+	/** @var string $address The address of the person */
 	public string $address;
 
-	function __construct($name, $address)
+	/**
+	 * Constructor method for the `Person` class.
+	 * 
+	 * @param string $name The name of the person
+	 * @param string $address The address of the person
+	 */
+	function __construct(string $name, string $address)
 	{
 		$this->name = $name;
 		$this->address = $address;
 	}
 
-	function set_address($address): void
+	/**
+	 * Set the address of the person.
+	 * 
+	 * @param string $address The new address of the person
+	 * @return void
+	 */
+	function set_address(string $address): void
 	{
 		$this->address = $address;
 	}
 
+	/**
+	 * Get the address of the person.
+	 * 
+	 * @return string The current address of the person
+	 */
 	function get_address(): string
 	{
 		return $this->address;
 	}
 }
 
+/**
+ * Class `Student` represents a student, which is a specific type of person.
+ */
 class Student extends Person
 {
+	/** @var string $program The program of the student */
 	public string $program;
+
+	/** @var int $year The year of study of the student */
 	public int $year;
+
+	/** @var float $fee The fee of the student */
 	public float $fee;
 
+	/**
+	 * Constructor method for the `Student` class.
+	 * 
+	 * @param string $name The name of the student
+	 * @param string $address The address of the student
+	 * @param string $program The program of the student
+	 * @param int $year The year of study of the student
+	 * @param float $fee The fee of the student
+	 */
 	function __construct(string $name, string $address, string $program, int $year, float $fee)
 	{
 		parent::__construct($name, $address);
@@ -36,34 +76,64 @@ class Student extends Person
 		$this->fee = $fee;
 	}
 
-	function set_program($program): void
+	/**
+	 * Set the program of the student.
+	 * 
+	 * @param string $program The new program of the student
+	 * @return void
+	 */
+	function set_program(string $program): void
 	{
 		$this->program = $program;
 	}
 
+	/**
+	 * Get the program of the student.
+	 * 
+	 * @return string The current program of the student
+	 */
 	function get_program(): string
 	{
 		return $this->program;
 	}
 
-
-
-	function set_year($year): void
+	/**
+	 * Set the year of study of the student.
+	 * 
+	 * @param int $year The new year of study of the student
+	 * @return void
+	 */
+	function set_year(int $year): void
 	{
 		$this->year = $year;
 	}
 
+	/**
+	 * Get the year of study of the student.
+	 * 
+	 * @return int The current year of study of the student
+	 */
 	function get_year(): int
 	{
 		return $this->year;
 	}
 
-
-	function set_fee($fee): void
+	/**
+	 * Set the fee of the student.
+	 * 
+	 * @param float $fee The new fee of the student
+	 * @return void
+	 */
+	function set_fee(float $fee): void
 	{
 		$this->fee = $fee;
 	}
 
+	/**
+	 * Get the fee of the student.
+	 * 
+	 * @return float The current fee of the student
+	 */
 	function get_fee(): float
 	{
 		return $this->fee;
@@ -73,33 +143,72 @@ class Student extends Person
 $student1 = new Student("John Doe", "123 Main St", "Computer Science", 3, 4000);
 echo "Student Name: " . $student1->name . "\n";
 
-class Staff
+/**
+ * Class `Staff` represents a staff member with a school and pay.
+ */
+class Staff extends Person
 {
+	/** @var string $school The school where the staff member works */
 	public string $school;
+
+	/** @var float $pay The pay of the staff member */
 	public float $pay;
 
+	/**
+	 * Constructor method for the `Staff` class.
+	 * 
+	 * @param string $name The name of the staff member
+	 * @param string $address The address of the staff member
+	 * @param string $school The school where the staff member works
+	 * @param float $pay The pay of the staff member
+	 */
 	function __construct(string $name, string $address, string $school, float $pay)
 	{
+		parent::__construct($name, $address);
 		$this->school = $school;
 		$this->pay = $pay;
 	}
 
-	function set_school($school): void
+	/**
+	 * Set the school where the staff member works.
+	 * 
+	 * @param string $school The new school of the staff member
+	 * @return void
+	 */
+	function set_school(string $school): void
 	{
 		$this->school = $school;
 	}
 
+	/**
+	 * Get the school where the staff member works.
+	 * 
+	 * @return string The current school of the staff member
+	 */
 	function get_school(): string
 	{
 		return $this->school;
 	}
-	function set_pay($pay): void
+
+	/**
+	 * Set the pay of the staff member.
+	 * 
+	 * @param float $pay The new pay of the staff member
+	 * @return void
+	 */
+	function set_pay(float $pay): void
 	{
 		$this->pay = $pay;
 	}
 
+	/**
+	 * Get the pay of the staff member.
+	 * 
+	 * @return float The current pay of the staff member
+	 */
 	function get_pay(): float
 	{
 		return $this->pay;
 	}
 }
+?>
